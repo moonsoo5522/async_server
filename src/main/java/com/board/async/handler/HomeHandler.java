220e19fd538f2c303d8ec09cc5eb9d733f0f07a3
moonsoo5522 <moonsoo5522@naver.com>
2020-01-25 03:15:39 +0900

    handler 수정

--- src/main/java/com/board/async/handler/HomeHandler.java
+++ src/main/java/com/board/async/handler/HomeHandler.java
@@ -25,9 +25,8 @@ public class HomeHandler {
     private MemberRepository memberRepository;
 
     public Mono<ServerResponse> hello(ServerRequest request) {
-        System.out.println(memberRepository.findByDeviceId("hello").getRegDt());
         return ServerResponse.ok()
-                  .body(boardRepository.findAll(), Board.class);
+                  .body(memberRepository.findByDeviceId("hello"), Member.class);
     }
 
     public Mono<ServerResponse> test(ServerRequest request) {
